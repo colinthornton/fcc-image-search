ba7b73b7b69819f818582ef4b52a00fbb315d90b
Colin Thornton <colinthorntontx@gmail.com>
2017-12-07 22:39:11 +0900

    Implement API request at root route

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const API_KEY = 'AIzaSyCAdpVRyNo83GCvpA1EXCU_oUJV3j3wmsY';
+const SEARCH_ENGINE_ID = '000418693802840694674:ldz-djbro9q';
+const URL = `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${SEARCH_ENGINE_ID}&searchType=image&q=`
+
+app.get('/:query', (req, res) => {
+  axios.get(URL + req.params.query)
+    .then(response => {
+      response.data.items.forEach(image => {
+        console.log(image.title);
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  res.send(URL + req.params.query);
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('Server running...'));
