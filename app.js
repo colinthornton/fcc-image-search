4c9ca318ce0d1a7994f6e2b78d59baecd97d9f78
Colin Thornton <colinthorntontx@gmail.com>
2017-12-08 20:08:12 +0900

    Add /recent route

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const axios = require('axios');
+const mongoose = require('mongoose');
 const app = express();
 
 const API_KEY = 'AIzaSyCAdpVRyNo83GCvpA1EXCU_oUJV3j3wmsY';
 const SEARCH_ENGINE_ID = '000418693802840694674:ldz-djbro9q';
 const URL = 'https://www.googleapis.com/customsearch/v1';
+const MLAB_URI = process.env.MLAB_URI;
+// const DEV_DB = 'mongodb://localhost:27017';
+
+mongoose.connect(MLAB_URI || DEV_DB, { useMongoClient: true });
+
+const searchSchema = new mongoose.Schema({
+  query: String,
+  time: String,
+});
+const Search = mongoose.model('Search', searchSchema);
 
 app.get('/api/:query', (req, res) => {
-  const json = [];
+  let time = new Date();
+  time = time.toString();
+  const newSearch = {
+    query: req.params.query,
+    time,
+  };
+  Search.create(newSearch, (err, search) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`New search "${search.query}" created.`);
+  });
+
   const params = {
     key: API_KEY,
     cx: SEARCH_ENGINE_ID,
@@ -20,6 +43,7 @@ app.get('/api/:query', (req, res) => {
 
   axios.get(URL, { params })
     .then(response => {
+      const json = [];
       response.data.items.forEach(image => {
         json.push({
           title: image.title,
@@ -31,9 +55,20 @@ app.get('/api/:query', (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.json({
-        error: true,
-      });
+      res.json({ error: true, });
+    });
+});
+
+app.get('/recent', (req, res) => {
+  Search.find({}, '-_id query time')
+    .sort({ _id: -1 })
+    .limit(10)
+    .exec((err, recentSearches) => {
+      if (err) {
+        console.log(err);
+        return res.json({ error: true });
+      }
+      res.json(recentSearches);
     });
 });
 
