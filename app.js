66b374adc93171e522a9bf0c61db1edfee32480b
Colin Thornton <colinthorntontx@gmail.com>
2017-12-08 16:08:18 +0900

    Complete API query route

--- app.js
+++ app.js
@@ -4,29 +4,37 @@ const app = express();
 
 const API_KEY = 'AIzaSyCAdpVRyNo83GCvpA1EXCU_oUJV3j3wmsY';
 const SEARCH_ENGINE_ID = '000418693802840694674:ldz-djbro9q';
-const URL = 'https://www.googleapis.com/customsearch/v1'
+const URL = 'https://www.googleapis.com/customsearch/v1';
 
-app.get('/:query', (req, res) => {
-  let params = {
+app.get('/api/:query', (req, res) => {
+  const json = [];
+  const params = {
     key: API_KEY,
     cx: SEARCH_ENGINE_ID,
     searchType: 'image',
-    q: req.params.query
+    q: req.params.query,
   }
   if (req.query.offset) {
     params.start = req.query.offset;
   }
-  
+
   axios.get(URL, { params })
     .then(response => {
       response.data.items.forEach(image => {
-        console.log(image.title);
+        json.push({
+          title: image.title,
+          url: image.link,
+          context: image.image.contextLink,
+        });
       });
+      res.json(json);
     })
     .catch(error => {
       console.log(error);
+      res.json({
+        error: true,
+      });
     });
-  res.send(URL + req.params.query);
 });
 
 app.listen(process.env.PORT || 8080, () => console.log('Server running...'));
